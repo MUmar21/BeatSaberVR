34ffdc0ab3dfe179e058e0c61df318bc671f529c
MUmar21 <96631305+MUmar21@users.noreply.github.com>
2026-03-26 16:09:47 +0500

    Updates

--- Assets/BeatSaberVR/Scripts/RaysBehavior/RaysBehavior.cs
+++ Assets/BeatSaberVR/Scripts/RaysBehavior/RaysBehavior.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class RaysBehavior : MonoBehaviour
 {
+    [Header("Ray Objects")]
+    [Tooltip("Drag all your individual Ray GameObjects (with MeshRenderers) here.")]
+    [SerializeField] private Renderer[] rayRenderers;
+
     [Header("Colors")]
     [SerializeField, ColorUsage(true, true)] private Color redColor = Color.red;
     [SerializeField, ColorUsage(true, true)] private Color blueColor = Color.blue;
@@ -12,14 +16,21 @@ public class RaysBehavior : MonoBehaviour
     public float fadeDuration = 0.15f;
     public float startAlpha = 0.8f;
 
-    private Material rayMaterial;
+    private Material[] rayMaterials;
+
     private static readonly int ColorProp = Shader.PropertyToID("_BaseColor");
     private static readonly int EmissionProp = Shader.PropertyToID("_EmissionColor");
 
     private void Awake()
     {
-        rayMaterial = GetComponent<Renderer>().material;
-        SetAlpha(0);
+        rayMaterials = new Material[rayRenderers.Length];
+
+        for (int i = 0; i < rayRenderers.Length; i++)
+        {
+            rayMaterials[i] = rayRenderers[i].material;
+            SetRayAlpha(i, 0f);
+            rayRenderers[i].gameObject.SetActive(false);
+        }
     }
 
     private void OnEnable()
@@ -30,35 +41,68 @@ public class RaysBehavior : MonoBehaviour
     private void OnDisable()
     {
         BeatSaberVREvents.OnBlockCut -= ShowRay;
-        DOTween.Kill(this.transform);
+
+        for (int i = 0; i < rayRenderers.Length; i++)
+        {
+            if (rayRenderers[i] != null)
+                DOTween.Kill(rayRenderers[i].transform);
+        }
     }
 
     private void ShowRay(BlockColor color)
     {
+        int availableIndex = GetAvailableRayIndex();
+
+        // If all rays are currently flashing
+        if (availableIndex == -1)
+        {
+            availableIndex = Random.Range(0, rayRenderers.Length);
+        }
+
         Color targetColor = (color == BlockColor.Red) ? redColor : blueColor;
 
-        rayMaterial.SetColor(ColorProp, targetColor);
-        rayMaterial.SetColor(EmissionProp, targetColor);
-        SetAlpha(startAlpha);
+        rayMaterials[availableIndex].SetColor(ColorProp, targetColor);
+        rayMaterials[availableIndex].SetColor(EmissionProp, targetColor);
+        SetRayAlpha(availableIndex, startAlpha);
 
-        Animate();
+        rayRenderers[availableIndex].gameObject.SetActive(true);
+        AnimateRay(availableIndex);
     }
 
-    private void Animate()
+    private int GetAvailableRayIndex()
     {
-        DOTween.Kill(this.transform);
-
-        DOVirtual.Float(startAlpha, 0f, fadeDuration, SetAlpha)
-            .SetEase((DG.Tweening.Ease)Ease.EaseInCubic)
-            .SetId(this.transform);
+        for (int i = 0; i < rayRenderers.Length; i++)
+        {
+            if (!rayRenderers[i].gameObject.activeSelf)
+            {
+                return i;
+            }
+        }
+        return -1;
     }
 
-    private void SetAlpha(float alpha)
+    private void AnimateRay(int index)
     {
-        Color c = rayMaterial.GetColor(ColorProp);
+        Transform rayTransform = rayRenderers[index].transform;
+
+        DOTween.Kill(rayTransform);
+
+        DOVirtual.Float(startAlpha, 0f, fadeDuration, (alpha) => SetRayAlpha(index, alpha))
+            .SetEase((DG.Tweening.Ease)Ease.EaseOutCubic)
+            .SetId(rayTransform)
+            .OnComplete(() =>
+            {
+                rayRenderers[index].gameObject.SetActive(false);
+            });
+    }
+
+    private void SetRayAlpha(int index, float alpha)
+    {
+        Material mat = rayMaterials[index];
+        Color c = mat.GetColor(ColorProp);
         c.a = alpha;
-        rayMaterial.SetColor(ColorProp, c);
+        mat.SetColor(ColorProp, c);
 
-        rayMaterial.SetColor(EmissionProp, c * Mathf.LinearToGammaSpace(alpha));
+        mat.SetColor(EmissionProp, c * Mathf.LinearToGammaSpace(alpha));
     }
 }
--- Assets/BeatSaberVR/Scripts/Utilities/LaneHelper/LaneHelper.cs
+++ Assets/BeatSaberVR/Scripts/Utilities/LaneHelper/LaneHelper.cs
@@ -5,10 +5,10 @@ namespace BeatSaberVR
     {
         // 4 columns: spaced 0.6 units apart, centered at x=0
         // col 0 = far left, col 3 = far right
-        private static readonly float[] colX = { -0.9f, -0.3f, 0.3f, 0.9f };
+        private static readonly float[] colX = { 0f, -0.3f, 0.3f, 0f };
 
         // 3 rows: bottom to top
-        private static readonly float[] rowY = { 1f, 1.3f, 1.6f };
+        private static readonly float[] rowY = { 0.4f, 0.55f, 0.7f };
 
         public static Vector3 GridToWorld(int col, int row, float zDepth)
         {
