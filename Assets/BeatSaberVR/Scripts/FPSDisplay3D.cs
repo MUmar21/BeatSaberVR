d773a5114b7a455809e0a9a6d6010ed96d47ee5a
YasirKhattak1987 <yasir__khattak@hotmail.com>
2026-04-14 15:16:14 +0500

    Lighting and other changes

---
+++ Assets/BeatSaberVR/Scripts/FPSDisplay3D.cs
@@ -0,0 +1,28 @@
+using TMPro;
+using UnityEngine;
+
+namespace GOSIVR
+{
+    public class FPSDisplay3D : MonoBehaviour
+    {
+        private float deltaTime;
+
+        private TextMeshPro _Text;
+
+        private void Start()
+        {
+            Application.targetFrameRate = 72;
+
+            _Text = GetComponent<TextMeshPro>();
+        }
+
+        private void LateUpdate()
+        {
+            deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
+
+            float fps = 1f / deltaTime;
+
+            _Text.text = fps.ToString("F2");
+        }
+    }
+}
